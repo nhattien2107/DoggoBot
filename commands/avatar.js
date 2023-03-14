992d9e9babe602b649642bc1f608684102247e63
Nguyen Nhat Tien <nnt2107@gmail.com>
2023-03-14 08:07:08 +0700

    basic commands

---
+++ commands/avatar.js
@@ -0,0 +1,23 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('avatar')
+		.setDescription('Get the avatar URL of the selected user, or your own avatar.')
+		.addUserOption(option => option.setName('target').setDescription('The user\'s avatar to show')),
+	async execute(interaction) {
+		const user = interaction.options.getUser('target');
+		const exampleEmbed = new EmbedBuilder()
+			.setColor(0x0099FF)											
+			.setFooter({ text: 'Doggo snitched the avatar for you.', iconURL: 'https://images-ext-2.discordapp.net/external/bYpErY_l2mK4tkJqdqm5vTo9xHMmmj0_2zPE4MQKhtI/https/cdn.discordapp.com/avatars/377932884687978506/0e72a95c4351562164bac0bfa152e17f.webp?width=160&height=160' });
+		if (user) {
+			exampleEmbed.setDescription(`${user.username}'s avatar:`);
+			exampleEmbed.setImage(`${user.displayAvatarURL()}`);
+		}
+		else {
+			exampleEmbed.setDescription(`Your avatar:`);
+			exampleEmbed.setImage(`${interaction.user.displayAvatarURL()}`);
+		}
+		interaction.reply({ embeds: [exampleEmbed] });
+	},
+};
